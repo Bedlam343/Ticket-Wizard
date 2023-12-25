19b4402e0a702b1fd70f265b14e8a3bd9494993c
bedlam343 <jagik22@gmail.com>
2023-12-25 13:59:18 -0800

    auth mongo db container

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors"; // This is a package that will automatically handle async errors without the need for next() or try/catch blocks
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -23,6 +24,18 @@ app.all("*", async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  try {
+    // "auth" is the name of the database
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
