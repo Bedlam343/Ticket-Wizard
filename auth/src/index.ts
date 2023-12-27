d89fdbc5b74f065193333f48e0875a898220b7dc
bedlam343 <jagik22@gmail.com>
2023-12-27 15:18:06 -0800

    jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors"; // This is a package that will automatically handle async errors without the need for next() or try/catch blocks
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -11,7 +12,14 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true); // trust traffic from nginx proxy
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // don't encrypt the cookie
+    secure: true, // only send the cookie over HTTPS
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
