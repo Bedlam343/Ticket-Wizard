7d7b51340ccebbe168425677ade8393e57a58e50
bedlam343 <jagik22@gmail.com>
2024-01-14 16:05:14 -0800

    test setup.ts

---
+++ auth/src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import "express-async-errors"; // This is a package that will automatically handle async errors without the need for next() or try/catch blocks
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set("trust proxy", true); // trust traffic from nginx proxy
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // don't encrypt the cookie
+    secure: true, // only send the cookie over HTTPS
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ auth/src/test/setup.ts
@@ -0,0 +1,30 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+let mongo: any;
+
+// hook function (executed before all tests started to be executed)
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+// runs before each test
+beforeEach(async () => {
+  // delete all collections
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if(mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
