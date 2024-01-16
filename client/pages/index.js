256e496c00aa0f0660d2b6c409cc539b051166d4
bedlam343 <jagik22@gmail.com>
2024-01-16 11:40:49 -0800

    getInitialProp request from server to different namespace

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,30 @@
-const Index = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-export default Index;
+// NEXT.js will call this function while rendering the app on the server
+LandingPage.getInitialProps = async () => {
+  // check if we are on the server or the browser (window object only exists inside browser)
+  if (typeof window === "undefined") {
+    // making request to a service in a different namespace
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
+
+// ingress-nginx-controller
+
+export default LandingPage;
