c8371e1119c932cbed8f3b31520acbb284a8f8e8
bedlam343 <jagik22@gmail.com>
2024-01-16 11:55:20 -0800

    landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,17 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed it</h1>
+  );
 };
 
 // NEXT.js will call this function while rendering the app on the server
 LandingPage.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get("/api/users/currentuser");
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
   return data;
 };
 
