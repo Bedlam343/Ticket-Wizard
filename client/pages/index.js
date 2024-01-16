9700517f583483a5e1e49742e260ee5d0cebadb9
bedlam343 <jagik22@gmail.com>
2024-01-16 11:43:35 -0800

    pass cookie in server getInitialProps to currentuser

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,16 +6,14 @@ const LandingPage = ({ currentUser }) => {
 };
 
 // NEXT.js will call this function while rendering the app on the server
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   // check if we are on the server or the browser (window object only exists inside browser)
   if (typeof window === "undefined") {
     // making request to a service in a different namespace
     const { data } = await axios.get(
       "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
       {
-        headers: {
-          Host: "ticketing.dev",
-        },
+        headers: req.headers,
       }
     );
     return data;
