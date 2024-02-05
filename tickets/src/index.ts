d56ac609a6f419fb2b938347d313b906202774b1
bedlam343 <jagik22@gmail.com>
2024-02-04 21:53:53 -0800

    nats singleton

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -12,7 +12,8 @@ const start = async () => {
   }
 
   try {
-    // "auth" is the name of the database
+    // connect to nats-depl
+    await natsWrapper.connect("ticketing", "lkasg", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
